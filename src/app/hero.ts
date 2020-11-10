aaf9f117e0460fd1cd68ec77ebf370b24bfa0d38
SimonCanecky <simoncanecky1@gmail.com>
2020-11-10 14:19:19 +0100

    Tour of Heroes - prepojenie Hero a Item, nedokoncene treba doladit mensie chyby

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,8 +1,8 @@
+
 import { Item } from './item';
-import { ItemsComponent } from './items/items.component';
-import { ITEMS } from './mock-items';
 
 export interface Hero {
     id: number;
     name: string;
+    items?: Item[];
     }
