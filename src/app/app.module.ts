b9103b61d8bd7c622c7990c18d66b7976915afbc
SimonCanecky <simoncanecky1@gmail.com>
2020-11-11 13:42:50 +0100

    Tour of Heroes - navigácia -urobena len 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { ItemDetailComponent } from './item-detail/item-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -18,11 +21,13 @@ import { MessagesComponent } from './messages/messages.component';
     ItemsComponent,
     HeroDetailComponent,
     ItemDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
